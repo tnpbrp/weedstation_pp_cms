a799b79edefad1e6c0b2ad4a1f81b58bd451d708
tnpbrp <burapartie@gmail.com>
2024-06-15 22:42:56 +0700

    initial commit

---
+++ assets/js/pages/dashbaord.page.js
@@ -0,0 +1,13 @@
+parasails.registerPage("dashboardpage", {
+    name: "dashboardpage",
+    props: {
+
+    },
+    data: {},
+    computed: {},
+    methods: {},
+    beforeMount: function () { },
+    mounted: function () { },
+    watch: {},
+
+});
