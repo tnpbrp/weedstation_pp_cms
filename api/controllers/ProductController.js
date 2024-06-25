6bbd8b81699115b2694a9b04dccbbc8fa241ef37
tnpbrp <burapartie@gmail.com>
2024-06-26 01:10:00 +0700

    update

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -6,7 +6,7 @@
  */
 
 module.exports = {
-    create: async function(req, res) {
+    create: async function (req, res) {
         try {
             let newData = await Product.create(req.body).fetch();
             return res.json(newData);
@@ -15,8 +15,8 @@ module.exports = {
         }
     },
 
-    find: async function(req, res) {
-        console.log(req)
+    find: async function (req, res) {
+        
         try {
             let allData = await Product.find();
             return res.json(allData);
@@ -25,8 +25,18 @@ module.exports = {
         }
     },
 
-    update: async function(req, res) {
-        console.log(req.params.id,req.body)
+    findOne: async function (req, res) {
+        try {
+            let allData = await Product.findOne({
+                id: req.param('id')
+            });
+            return res.json(allData);
+        } catch (err) {
+            return res.serverError(err);
+        }
+    },
+
+    update: async function (req, res) {
         try {
             let updatedData = await Product.updateOne({ id: req.params.id }).set(req.body);
             return res.json(updatedData);
@@ -35,7 +45,7 @@ module.exports = {
         }
     },
 
-    destroy: async function(req, res) {
+    destroy: async function (req, res) {
         try {
             let deletedData = await Product.destroyOne({ id: req.params.id });
             return res.json(deletedData);
@@ -43,8 +53,8 @@ module.exports = {
             return res.serverError(err);
         }
     },
-    
-    
+
+
 
 };
 
--- assets/js/pages/product.page.js
+++ assets/js/pages/product.page.js
@@ -5,19 +5,34 @@ parasails.registerPage("productpage", {
     },
     data: {
         newProduct: {},
-        editProduct:{},
-        rows:[],
+        productDetail: {},
+        editProduct: {},
+        rows: [],
         columns: [],
 
     },
     computed: {},
     methods: {
         findProducts() {
-            var vm =  this;
-            axios.get(`/product/find`).then(res => {
-                vm.rows = res.data
+            var vm = this;
+            let desiredKeys = ['id', 'number', 'name', 'price', 'action'];
+            let filteredData = {};
 
-                console.log(res.data)
+            axios.get(`/product/find`).then(res => {
+
+                res.data.map((object, index) => {
+                    object.number = index + 1;
+                })
+
+                desiredKeys.forEach(key => {
+                    if (res.data[0].hasOwnProperty(key)) {
+                        filteredData[key] = res.data[0][key];
+                    }
+                });
+
+                console.log(filteredData)
+                vm.rows.push(filteredData)
+
             }).catch(err => console.log(err));
         },
 
@@ -27,16 +42,57 @@ parasails.registerPage("productpage", {
             })
         },
 
+        viewProduct(productId) {
+            let vm = this;
+            console.log(productId)
+            //previos set id to view
+            axios.get(`/product/detail/${productId}`).then(res => {
+                console.log(res.data)
+                vm.productDetail = res.data;
+                $('#viewProductModal').modal('toggle');
+            }).catch((err) => {
+                console.log(err)
+            })
+
+
+        },
+
         updateProduct() {
             axios.put(`/product/update/`, {}).then((res) => {
                 console.log(res)
             })
         },
 
-        deleteProduct() {
-            axios.delete(`/product/delete/${id}`).then((res) => {
-                console.log(res)
-            })
+        deleteProduct(productId, productName) {
+            let vm = this;
+
+            $.confirm({
+                animation: 'zoom',
+                closeAnimation: 'zoom',
+                title: 'Delete\xa0' + productName,
+                content: '<div class="text-center">' +
+                    '<i class="far fa-trash-alt fa-4x mb-4 animated rotateIn"></i>' +
+                    '<p class="heading">Are you sure?, Do you want to delete.</p>' +
+                    '</div>',
+                buttons: {
+                    delete: {
+                        btnClass: 'btn btn-sm btn-danger',
+                        action: function () {
+                            axios.delete(`/product/delete/${productId}`).then((res) => {
+                                // after save success  reload data new
+                                vm.findProducts();
+                                $.notify('Deleted\xa0' + productName, 'success');
+                            })
+
+                        }
+                    },
+                    cancel: {
+                        btnClass: 'btn btn-sm btn-link',
+                    }
+                }
+            });
+
+
         },
 
         addProductModal() {
@@ -57,12 +113,14 @@ parasails.registerPage("productpage", {
                 }
 
                 form.classList.remove('was-validated');
-           
+
                 axios.post(`/product/create`, vm.newProduct).then(res => {
                     $('#addProductModal').modal('hide');
                     $.notify('Create product successfully.', 'success');
 
                     // after save success  reload data new
+                    vm.findProducts();
+
                 }).catch(err => console.log(err));
             });
 
@@ -72,6 +130,40 @@ parasails.registerPage("productpage", {
     },
     beforeMount: function () { },
     mounted: function () {
+        this.columns = [
+            {
+                label: 'ID',
+                field: 'id',
+                // width:'50'
+            },
+            {
+                label: 'No',
+                field: 'number',
+                width: '40'
+            },
+            {
+                label: 'Name',
+                field: 'name',
+                width: '100'
+
+            },
+            {
+                label: 'Price',
+                field: 'price',
+                width: '80'
+            },
+            // {
+            //     label: 'Image',
+            //     field: 'img',
+            //     width:'50'
+            // },
+            {
+                label: 'Actions',
+                field: 'action',
+                width: '100'
+            }
+        ];
+
         this.findProducts();
 
     },
